8e33530b731297f628a1f6e3be197a13a34d0657
Takuto Ike <ike@yukari.cta.kuee.kyoto-u.ac.jp>
2026-02-04 17:27:13 +0900

    save

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ class GlobalParams:
     load_file="data.dataset_branch"
     out_channels=1 #出力は1channel
     lr=1e-3 #学習率
-    num_epoch=500 #epoch数
+    num_epoch=5000 #epoch数
     batch_size=50 #バッチサイズ
     train_ratio=0.8
     #loss=Loss_reg+lambda_balance*loss_class
     #回帰用lossとlabellossの割合
     lambda_balance=0.2 
     seed=42 #randomのシード
-    seed_for_torch=17 #重み初期化用のseed値
+    seed_for_torch=5 #重み初期化用のseed値
     mask_ratio=1 #maskなしのときは1に
